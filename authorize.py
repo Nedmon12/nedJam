3ee33c80228d2dc06edd9be9faa82aaa9d3c5b71
Nedmon12 <mentesnot.ertibu@gmail.com>
2023-07-20 07:57:10 +0300

    join phonecall not for bots

--- authorize.py
+++ authorize.py
@@ -2,10 +2,11 @@ from pyrogram import Client
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
-bot_token = os.getenv('BOT_TOKEN')
+# bot_token = os.getenv('BOT_TOKEN')
 
-app = Client ("mybotsess", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+app = Client ("mybotsess", api_id=api_id, api_hash=api_hash)
 
 app.run()
