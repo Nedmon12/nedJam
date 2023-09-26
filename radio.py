b4a661480ed44860938c71967495afd57ab287fe
Nedmon12 <mentesnot.ertibu@gmail.com>
2023-09-26 09:28:43 +0300

    wrapped in event loop

--- radio.py
+++ radio.py
@@ -5,10 +5,16 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 import download
 import search
+import asyncio
+import pyrogram
 
 from pytgcalls import GroupCallFactory
 
-main_filter = filters.text & filters.outgoing & ~filters.edited
+SESSION = os.getenv('SESSION')
+API_HASH = os.getenv('API_HASH')
+API_ID = os.getenv('API_ID')
+
+main_filter = filters.text & filters.outgoing
 cmd_filter = lambda cmd: filters.command(cmd, prefixes='!')
 
 group_call = None
@@ -25,7 +31,7 @@ def init_client_and_delete_message(func):
     
     return wrapper
 
-@Client.on_message(main_filer & cmd_filter('play'))
+@Client.on_message(main_filter & cmd_filter('play'))
 async def start_playout(_, message: Message):
     if not message.reply_to_message or not message.reply_to_message.audio:
         return await message.delete()
@@ -40,3 +46,15 @@ async def start_playout(_, message: Message):
     print(f"this is message.text {message.text}")
     audio_original = await download.sendTitleandId(search.sendTitleandId(message.text))
     print(audio_original)
+
+async def main(client):
+    await client.start()
+    while not client.is_connected:
+        await asyncio.sleep(1)
+
+    await pyrogram.idle()
+
+if __name__ == '__main__':
+    main_client = pyrogram.Client(SESSION, int(API_ID), API_HASH)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(main_client))
